fffb80edd76d23f280245a697b1ae0a81a9e0dee
Hieuk <89458362+hieuknguyen@users.noreply.github.com>
2025-03-04 15:56:17 +0700

    Add files via upload

---
+++ settings.gradle.kts
@@ -0,0 +1,23 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "CN22F_NguyenTrungHieu_BaiTapTuan1"
+include(":app")
